3bfb97dbec4ae3c50e493b06e2890766c61012f0
S-Lombardi <silo.lombardi@gmail.com>
2023-07-31 17:33:07 +0200

    Richiesta feedback

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+//collego CONTROLLER
+use App\Http\Controllers\ComicController as ComicController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [ComicController::class, 'index'])->name('home');
+
+Route::resource('comics', ComicController::class);
