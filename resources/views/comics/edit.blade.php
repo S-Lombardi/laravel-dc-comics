50dd2a866241048f641fe730fb71133ab6b19e80
S-Lombardi <silo.lombardi@gmail.com>
2023-08-01 19:00:10 +0200

    Creato file edit

---
+++ resources/views/comics/edit.blade.php
@@ -0,0 +1,46 @@
+@extends ('layouts.app')
+
+{{-- FORM - MODIFICA DAATI IN INPUT--}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <form action="" method="POST">
+                    @csrf
+                    @method('PUT')
+                    {{-- TITOLO --}}
+                    <div class="form-group">
+                        <label for="" class="control-label">Titolo</label>
+                        <input type="text"class="form-control" id="title" name="title" placeholder="titolo">
+                    </div>
+                    {{-- IMG --}}
+                    <div class="form-group">
+                        <label for="" class="control-label">Immagine</label>
+                        <input type="text"class="form-control" id="thumb" name="thumb" placeholder="Copertina fumetto">
+                    </div>
+                    {{-- SERIE --}}
+                    <div class="form-group">
+                        <label for="" class="control-label">Serie</label>
+                        <input type="text"class="form-control" id="series" name="series" placeholder="Serie">
+                    </div>
+                    {{-- PREZZO --}}
+                    <div class="form-group">
+                        <label for="" class="control-label">Prezzo</label>
+                        <input type="text" class="form-control"id="price" name="price" placeholder="Prezzo">
+                    </div>
+                    {{-- TIPO --}}
+                    <div class="form-group">
+                        <label for="" class="control-label">Tipo</label>
+                        <input type="text"class="form-control" id="type" name="type" placeholder="Tipo">
+                    </div>
+                    {{-- PULSANTE --}}
+                    <div class="form-group">
+                        <button class="btn btn-success mt-3" type="submit"class="form-control">Salva</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </div>
+
+@endsection
