16a45188897221179549933e2d9fc52a54fc42e3
S-Lombardi <silo.lombardi@gmail.com>
2023-08-03 00:14:18 +0200

    Tolti @error da create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -25,41 +25,31 @@
                     <div class="form-group">
                         <label for="" class="control-label">Titolo</label>
                         <input type="text"class="form-control" id="title" name="title" placeholder="titolo">
-                        @error('title')
-                            <div class="invalid-feedback"> {{$message}}</div>
-                        @enderror
+                        
                     </div>
                     {{-- IMG --}}
                     <div class="form-group">
                         <label for="" class="control-label">Immagine</label>
                         <input type="text"class="form-control" id="thumb" name="thumb" placeholder="Copertina fumetto">
-                        @error('thumb')
-                            <div class="invalid-feedback"> {{$message}}</div>
-                        @enderror
+                       
                     </div>
                     {{-- SERIE --}}
                     <div class="form-group">
                         <label for="" class="control-label">Serie</label>
                         <input type="text"class="form-control" id="series" name="series" placeholder="Serie">
-                        @error('series')
-                            <div class="invalid-feedback"> {{$message}}</div>
-                        @enderror
+                        
                     </div>
                     {{-- PREZZO --}}
                     <div class="form-group">
                         <label for="" class="control-label">Prezzo</label>
                         <input type="text" class="form-control"id="price" name="price" placeholder="Prezzo">
-                        @error('price')
-                            <div class="invalid-feedback"> {{$message}}</div>
-                        @enderror
+                        
                     </div>
                     {{-- TIPO --}}
                     <div class="form-group">
                         <label for="" class="control-label">Tipo</label>
                         <input type="text"class="form-control" id="type" name="type" placeholder="Tipo">
-                        @error('type')
-                            <div class="invalid-feedback">{{$message}}</div>
-                        @enderror
+                        
                     </div>
                     {{-- PULSANTE --}}
                     <div class="form-group">
